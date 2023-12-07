71caa8eea48d9f4ae4fc2565cc29d9da4f25a9d7
pabotesu <pab0te2u@gmail.com>
2023-12-08 01:39:07 +0900

    update i18n

--- hosts/thinkpad_x280/nixos.nix
+++ hosts/thinkpad_x280/nixos.nix
@@ -14,6 +14,7 @@
       ./hardware-configuration.nix
       
       ../../modules/nix_config.nix
+      ../../modules/i18n.nix
     ];
 
   # Bootloader.
@@ -31,22 +32,22 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Asia/Tokyo";
+  #time.timeZone = "Asia/Tokyo";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  #i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+  #i18n.extraLocaleSettings = {
+  #  LC_ADDRESS = "en_US.UTF-8";
+  #  LC_IDENTIFICATION = "en_US.UTF-8";
+  #  LC_MEASUREMENT = "en_US.UTF-8";
+  #  LC_MONETARY = "en_US.UTF-8";
+  #  LC_NAME = "en_US.UTF-8";
+  #  LC_NUMERIC = "en_US.UTF-8";
+  #  LC_PAPER = "en_US.UTF-8";
+  #  LC_TELEPHONE = "en_US.UTF-8";
+  #  LC_TIME = "en_US.UTF-8";
+  #};
 
   # Configure keymap in X11
   services.xserver = {
@@ -62,16 +63,6 @@
     packages = with pkgs; [];
   };
   
-  # nix settings
-  nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-    };
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
--- modules/nix_config.nix
+++ modules/nix_config.nix
@@ -1,15 +1,15 @@
 {username, ...}: {
   nix = {
     settings = {
-      auto-optimise = true;
+      auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "${username}"];
-      accept=flake-config = true;
+      accept-flake-config = true;
     };
     gc = {
       automatic = true;
       dates = "weekly";
-      optinos = "--delete-older-than 7d";
+      options = "--delete-older-than 7d";
     };
   };
   nixpkgs.config.allowUnfree = true;
