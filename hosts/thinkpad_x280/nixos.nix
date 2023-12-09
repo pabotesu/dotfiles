ea8ada9ff59aeab34c4ad44e0a01c78a071ce2d2
pabotesu <pab0te2u@gmail.com>
2023-12-09 16:34:00 +0900

    update networking

--- hosts/thinkpad_x280/nixos.nix
+++ hosts/thinkpad_x280/nixos.nix
@@ -28,27 +28,6 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  #time.timeZone = "Asia/Tokyo";
-
-  # Select internationalisation properties.
-  #i18n.defaultLocale = "en_US.UTF-8";
-
-  #i18n.extraLocaleSettings = {
-  #  LC_ADDRESS = "en_US.UTF-8";
-  #  LC_IDENTIFICATION = "en_US.UTF-8";
-  #  LC_MEASUREMENT = "en_US.UTF-8";
-  #  LC_MONETARY = "en_US.UTF-8";
-  #  LC_NAME = "en_US.UTF-8";
-  #  LC_NUMERIC = "en_US.UTF-8";
-  #  LC_PAPER = "en_US.UTF-8";
-  #  LC_TELEPHONE = "en_US.UTF-8";
-  #  LC_TIME = "en_US.UTF-8";
-  #};
-
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
