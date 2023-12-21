c4ad3881eb2454014ad914c99380285a6a305e73
pabotesu <pab0te2u@gmail.com>
2023-12-21 22:55:14 +0900

    fix screen_lock

--- home-manager/desktop_systems/screen_lock.nix
+++ home-manager/desktop_systems/screen_lock.nix
@@ -3,5 +3,5 @@
   ...
 }: {
   home.packages = [pkgs.swaylock-effects];
-  home.file.".config/swaylock/config".text = ./config/swaylock.config.nix 
+  home.file.".config/swaylock/config".text = (import ./config/swaylock.config.nix); 
 }
