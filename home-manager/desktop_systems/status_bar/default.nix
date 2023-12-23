9236020bfc7b6deda01d65199b26f42849ad6a89
pabotesu <pab0te2u@gmail.com>
2023-12-23 18:49:11 +0900

    add waybar config

--- home-manager/desktop_systems/status_bar/default.nix
+++ home-manager/desktop_systems/status_bar/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   programs.waybar = {
     enable = true;
-    configDir = ./config;
   };
+  home.file.".config/waybar/config".source = ./config/waybar.config;
+  home.file.".config/waybar/style.css".source = ./config/style.css;
 }
