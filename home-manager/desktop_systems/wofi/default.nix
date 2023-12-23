0964bc8383d6430b28119546a58afb99f9b4f32d
pabotesu <pab0te2u@gmail.com>
2023-12-23 13:48:57 +0900

    add powermenu

--- home-manager/desktop_systems/wofi/default.nix
+++ home-manager/desktop_systems/wofi/default.nix
@@ -8,7 +8,7 @@ home.file.".config/wofi/config".source = ./config/wofi.config;
 home.file.".config/wofi/style.css".source = ./config/css/style.css;
 
 ## power menu
-home.file.".config/wofi/powermenu.config".souce = ./config/powermenu.config;
+home.file.".config/wofi/powermenu.config".source = ./config/powermenu.config;
 home.file.".config/wofi/powermenu.css".source = ./config/css/powermenu.css;
 home.file.".rsc_dir/powermenu.sh".source = ./script/powermenu.sh;
 }
