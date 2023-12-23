7c73c3bd116176a54e2d0c2d6cd8859964a1c5fe
pabotesu <pab0te2u@gmail.com>
2023-12-23 13:28:03 +0900

    add launcher

--- home-manager/desktop_systems/wofi/default.nix
+++ home-manager/desktop_systems/wofi/default.nix
@@ -2,7 +2,7 @@
   programs.wofi = {
     enable = true;
   };
-}
+
 ## app launcher
 home.file.".config/wofi/config".source = ./config/wofi.config;
 home.file.".config/wofi/style.css".source = ./config/css/style.css;
@@ -11,3 +11,4 @@ home.file.".config/wofi/style.css".source = ./config/css/style.css;
 home.file.".config/wofi/powermenu.conf".souce = ./config/powermenu.conf;
 home.file.".config/wofi/powermenu.css".source = ./config/css/powermenu.conf;
 home.file.".rsc_dir/powermenu.sh".source = ./script/powermenu.sh;
+}
