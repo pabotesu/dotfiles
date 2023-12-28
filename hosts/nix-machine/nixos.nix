f73d94005a474c314b4c40f59f4d064c73acca3d
pabotesu <pab0te2u@gmail.com>
2023-12-28 22:40:59 +0900

    add hosts

---
+++ hosts/nix-machine/nixos.nix
@@ -0,0 +1,63 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ 
+  inputs,
+  pkgs,
+  hostname,
+  username, 
+  ...
+}: {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      
+      ../../modules/nix_config.nix
+      ../../modules/i18n.nix
+      ../../modules/networking.nix
+      ../../modules/xserver.nix
+      ../../modules/wm.nix
+      ../../modules/dm.nix
+
+    ]
+    ++ (with inputs.nixos-hardware.nixosModules; [
+      common-cpu-amd
+      common-gpu-amd
+      common-pc-ssd
+    ]);
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  };
+
+  networking.hostName = hostname; 
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pabotesu = {
+    isNormalUser = true;
+    description = "pabotesu";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   vim
+   wget
+   git
+   bottom
+  ];
+
+  # enable ssh
+  services.openssh.enable = true; 
+
+  # Did you read the comment?
+  system.stateVersion = "23.05";
+}
+
