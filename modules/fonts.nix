8fbd21418b6e6475764d7b36f51c502edbb9652b
pabotesu <pab0te2u@gmail.com>
2023-12-29 13:42:51 +0900

    add fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -7,6 +7,41 @@
       migu
       nerdfonts
     ];
-    
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Noto Serif CJK JP"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+           "Noto Serif CJK JP"
+           "Noto Color Emoji"
+        ];
+        monospase = [
+          "JetBrainsMono Nerd Font"
+          ""Noto Color Emoji"
+        ];
+        emoji = ["Noto Color Emoji"];
+      };
+    };
+    localConf = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+      <fontconfig>
+        <descrpsion>Change default fonts for Steam Client</description>
+        <match>
+          <test name="prgname">
+            <string>steamwebhelper</string>
+          </test>
+          <test name="family" qual="any">
+            <string>sans-serif</string>           
+          </test>
+          <edit>
+            <string>Migu 1P</string>
+          </edit>
+        </match>
+      </fontconfig>
+    '';
   };
 }
