0580ae22682cb4144c1981756c071fb7b0a89f15
pabotesu <pab0te2u@gmail.com>
2023-12-31 16:21:49 +0900

    import alacritty color scheme iceberg

--- home-manager/gui_apps/terminal/default.nix
+++ home-manager/gui_apps/terminal/default.nix
@@ -1,4 +1,6 @@
-{
+{theme, ...}: let
+  xcolors = theme.xcolors;
+in {
   programs.alacritty = {
     enable = true;
       settings = {
@@ -10,22 +12,22 @@
         };
         font = {
           size = 12;
-          ### fonts ###
+          normal.family = "JetBrainsMono Nerd Font";
         };
         colors = {
           primary = {
-            background = "0x1d1f21";
-            foreground = "0xc5c8c6";
+            background = xcolors.bg;
+            foreground = xcolors.bg;
           };
           normal = {
-            black   = "0x282a2e";
-            red     = "0xcc6666";
-            green   = "0xb5bd68";
-            yellow  = "0xf0c674";
-            blue    = "0x81a2be";
-            magenta = "0xb294bb";
-            cyan    = "0x8abeb7";
-            white   = "0xc5c8c6";
+            inherit (xcolors) black;
+            inherit (xcolors) red;
+            inherit (xcolors) green;
+            inherit (xcolors) yellow;
+            inherit (xcolors) blue;
+            inherit (xcolors) magenta;
+            inherit (xcolors) cyan;
+            inherit (xcolors) white;
           };
         };
       };
