344bced468677075a98a30eb10a47ae6dd87c65a
pabotesu <pab0te2u@gmail.com>
2023-12-31 16:15:37 +0900

    add color scheme iceberg

---
+++ themes/colors/iceberg/default.nix
@@ -0,0 +1,13 @@
+{
+  bg = "#161821";
+  fg = "#c6c8d1";
+
+  black = "#1e2132";
+  red = "#e27878";
+  green = "#b4be82";
+  yellow = "#e2a478";
+  blue = "#84a0c6";
+  magenta = "#a093c7";
+  cyan = "#89b8c2";
+  white = "#c6c8d1";
+}
