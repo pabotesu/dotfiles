04d98b57b24f9c2bb7405c9faf515d26910b8ffd
pabotesu <pab0te2u@gmail.com>
2024-01-04 19:38:10 +0900

    fix fcitx

--- modules/fcitx5.nix
+++ modules/fcitx5.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   i18n.inputMethod = {
     enabled = "fcitx5";
+    fcitx.engines = with pkgs.fcitx-engines; [ mozc ];
     fcitx5.addons = with pkgs; [fcitx5-mozc fcitx5-gtk];
   };
 }
