d46313de7f69fa2b879b0e8d0497689ea80ec3e6
pabotesu <pab0te2u@gmail.com>
2024-01-13 16:04:33 +0900

    update

--- home-manager/cli_apps/git.nix
+++ home-manager/cli_apps/git.nix
@@ -17,5 +17,6 @@
     nodePackages.gitmoji-cli
     ghq
     peco
+    hub
   ];
 }
--- home-manager/cli_apps/shell/default.nix
+++ home-manager/cli_apps/shell/default.nix
@@ -17,10 +17,10 @@
     initExtra = 
       ''
         # setup peco & ghq
-        function peco-src () {
+        function peco-src() {
           local selected_dir=$(ghq list -p | peco --prompt="repositories >" --query "$LBUFFER")
           if [ -n "$selected_dir" ]; then
-            BUFFER="cd ${selected_dir}"
+            BUFFER="cd $selected_dir"
             zle accept-line
           fi
           zle clear-screen
